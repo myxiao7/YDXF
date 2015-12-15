46e180ce1a767d303b3ecfd8d3c73f4678f9b26e
My灬xiao7 <494643819@qq.com>
2015-12-15 17:18:34 +0800

    Initial commit

---
+++ app/src/main/java/com/sizhuo/ydxf/Splash.java
@@ -0,0 +1,15 @@
+package com.sizhuo.ydxf;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
